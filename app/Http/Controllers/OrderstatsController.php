584de106438d35d0e983e5e920a3160c9a7e4234
Elliot Grabish <egrabish@gmail.com>
2015-06-17 17:57:58 -0400

    order stats beginnings

---
+++ app/Http/Controllers/OrderstatsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\order;
+use App\order_item;
+use App\orderstatsClass;
+
+use Illuminate\Support\Facades\DB;
+use Request;
+
+class OrderstatsController extends Controller {
+
+
+}
---
+++ app/orderstatsClass.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App;
+
+use Illuminate\Support\Facades\DB;
+use App\order;
+use App\order_item;
+
+class orderstatsClass {
+
+
+
+}
