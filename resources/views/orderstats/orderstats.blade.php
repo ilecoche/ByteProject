ce373c97a8395b2795cf0576d2db5b8ecfd27a42
Elliot Grabish <egrabish@gmail.com>
2015-06-18 11:42:30 -0400

    stats rough done

--- resources/views/orderstats/orderstats.blade.php
+++ resources/views/orderstats/orderstats.blade.php
@@ -1,19 +1,39 @@
 <h2>Best Selling Items</h2>
 
 <ol>
-@foreach($items as $item)
+	@foreach($items as $item)
 
-		<li>{{ $item->menu_item }} | Quantity: {{ $item->totalqty }}</li>
+			<li>{{ $item->menu_item }} | Quantity: {{ $item->totalqty }}</li>
 
-@endforeach
+	@endforeach
 </ol>
 
 <h2>Best Selling Days In The Last Week</h2>
+<p>Between Today and Last <?php echo $days['week_ago_day']; ?> (<?php echo $days['week_ago']; ?>)</p>
 
 <ol>
-@foreach($days as $day)
+	@foreach($days['days'] as $day)
 
-		<li><?php echo date('l', strtotime($day->date)); ?> | Total: ${{ $day->total }}</li>
+			<li><?php echo date('l', strtotime($day->date)); ?> | Total: ${{ $day->total }}</li>
 
-@endforeach
+	@endforeach
 </ol>
+
+<h2>Last Week Tip Totals</h2>
+<p>Between Sunday (<?php echo $tips['week_start']; ?>) and Saturday (<?php echo $tips['week_end']; ?>)</p>
+
+<ol>
+	@foreach($tips['tips'] as $tip)
+
+			<li><?php echo date('l', strtotime($tip->date)); ?> | Total: ${{ $tip->tiptotal }}</li>
+
+	@endforeach
+</ol>
+
+Tip Total: $<?php echo $tips['totalTip'][0]->tiptotalfull; ?>
+
+<h2>Last Week Total Revenue</h2>
+<p>Between Sunday (<?php echo $tips['week_start']; ?>) and Saturday (<?php echo $tips['week_end']; ?>)</p>
+
+Total: $<?php echo $total[0]->totalfull; ?>
+
