2dbc5f8f09b6b0a3ff8d80def85d39fec3e8336d
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-06-19 00:15:18 -0400

    More form functionality and styling

--- resources/views/reservation/tables.blade.php
+++ resources/views/reservation/tables.blade.php
@@ -48,9 +48,6 @@
 
 @section('additionalscripts')
 
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-    <!--<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>-->
     <script src="{{ asset('js/tables.js') }}" type="text/javascript"></script>
 
 @stop
