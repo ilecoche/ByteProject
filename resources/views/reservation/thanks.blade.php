b6118995997710310b734641540a0ad3c6a8878b
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-06-21 18:29:46 -0400

    date validation with bootstrap date picker

--- resources/views/reservation/thanks.blade.php
+++ resources/views/reservation/thanks.blade.php
@@ -1,7 +1,8 @@
 <div class="row thanks-container">
 	<div class="md-col-6">
 		<h1>Thank you! <i class="fa fa-check-circle"></i></h1>
-		<p>A reservation has been booked under the name, <strong>{{ $data['fname'] }} {{ $data['lname'] }}</strong>. Please review the details below and <a href="#">contact us</a> if you need further assistance.</p>
+		<p>A reservation has been booked under the name, <strong>{{ $data['fname'] }} {{ $data['lname'] }}</strong>.</p>
+		<p>Please review the details below and <a href="#">contact us</a> if you need further assistance.</p>
 
 		<div class="thanks-details">
 			<p>Reservation Date and Time: <strong>{{ $data['date'] }}</strong> @ <strong>{{ date("g:i a",strtotime($data['time'])) }}</strong></p>
