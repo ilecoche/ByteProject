5fc644719ca9399d6b96a53f554f8534b2b497db
jordvn <jhordon.j.king@gmail.com>
2015-06-24 23:46:43 -0400

    css

--- resources/views/payment/process.blade.php
+++ resources/views/payment/process.blade.php
@@ -5,13 +5,14 @@
 
 @stop
 @section('content')
-<div class="container">
-    <!-- sucess page for payment -->
-    <h1>Credit Card payment was successful</h1>
-    
-    <!-- Retrieves the name and email of costumer -->
-    <p>Thank you {{$bill->customer_name}}. We have sent an receipt of your purchase at {{$email}}</p>
-    <p>Come back soon</p>
+<div class="container-fluid">
+    <div id="paid"
+    <div class="panel ">
+        <div class="panel-heading SR"><h2>Credit Card payment was successful</h2></div>
+        <div class="panel-body"><p>Thank you {{$bill->customer_name}}. We have sent an receipt of your purchase at<strong> {{$email}} </strong></p>
+    <p>Come back soon</p></div>
+    </div>     
+    </div>
 </div>
 @stop
 @section('additionalscripts')
